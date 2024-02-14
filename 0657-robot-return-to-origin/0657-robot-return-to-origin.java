590cb5a52cdcb27247773edb98024fe6a81457ec
D6-80428-pradeep <125987234+D6-80428-pradeep@users.noreply.github.com>
2024-02-14 21:09:04 +0530

    Time: 11 ms (13.34%), Space: 43.7 MB (96.95%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+
+        int x=0;
+        int y=0;
+
+        for(int i=0;i<moves.length();i++){
+            if(moves.charAt(i)=='U'){
+                y++;
+            }
+            if(moves.charAt(i)=='D'){
+                y--;
+            }
+            if(moves.charAt(i)=='L'){
+                x--;
+            }
+            if(moves.charAt(i)=='R'){
+                x++;
+            }
+        }
+        if(x==0 && y==0){
+            return true;
+        }
+        return false;
+    }
+}
