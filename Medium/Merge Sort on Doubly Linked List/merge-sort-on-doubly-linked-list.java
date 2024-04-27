432393f8afd9a92a623543a0ec98d06cf32960ec
PRADEEP SINGH <125987234+D6-80428-pradeep@users.noreply.github.com>
2024-04-27 09:29:38 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge Sort on Doubly Linked List/merge-sort-on-doubly-linked-list.java
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next, prev;
+
+    Node(int key) {
+        data = key;
+        next = prev = null;
+    }
+}
+
+class Driverclass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node temp = head;
+
+            for (int i = 1; i < n; i++) {
+                int a = sc.nextInt();
+                Node n1 = new Node(a);
+                n1.prev = temp;
+                temp.next = n1;
+                temp = n1;
+            }
+
+            head = new Solution().sortDoubly(head);
+            printList(head);
+        }
+    }
+
+    public static void printList(Node node) {
+        Node temp = node;
+        while (node != null) {
+            System.out.print(node.data + " ");
+            temp = node;
+            node = node.next;
+        }
+        System.out.println();
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.prev;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next, prev;
+    Node(int data)
+    {
+        this.data = data;
+        next = prev = null;
+    }
+}
+*/
+class Solution {
+    // Function to sort the given doubly linked list using Merge Sort.
+    static Node sortDoubly(Node head) {
+        
+        List<Integer>list=new ArrayList<>();
+        Node temp=head;
+        while(temp!=null){
+            list.add(temp.data);
+            temp=temp.next;
+        }
+        Collections.sort(list);
+        Node ans=new Node(list.get(0));
+        temp=ans;
+        
+        for(int i=1;i<list.size();i++){
+             Node newnode=new Node(list.get(i));
+             temp.next=newnode;
+             newnode.prev=temp;
+             temp=temp.next;
+        }
+        return ans;
+    }
+}
