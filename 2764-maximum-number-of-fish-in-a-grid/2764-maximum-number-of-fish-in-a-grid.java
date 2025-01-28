8e41abfdd80f58244fe5ce469f9fcc07b08cc655
PRADEEP SINGH <125987234+D6-80428-pradeep@users.noreply.github.com>
2025-01-28 11:48:47 +0530

    Time: 7 ms (24.93%), Space: 45.5 MB (7.56%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
@@ -4,33 +4,35 @@ class Solution {
         int n = grid[0].length;
         int maxFish = 0;
         boolean[][] visited = new boolean[m][n];
+        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        Queue<int[]> queue = new LinkedList<>();
         
-        // Try each cell as starting point
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(grid[i][j] > 0 && !visited[i][j]) {
-                    maxFish = Math.max(maxFish, dfs(grid, i, j, m, n, visited));
+                    int currentFish = 0;
+                    queue.offer(new int[]{i, j});
+                    visited[i][j] = true;
+                    
+                    while(!queue.isEmpty()) {
+                        int[] curr = queue.poll();
+                        currentFish += grid[curr[0]][curr[1]];
+                        
+                        for(int[] dir : dirs) {
+                            int newI = curr[0] + dir[0];
+                            int newJ = curr[1] + dir[1];
+                            
+                            if(newI >= 0 && newI < m && newJ >= 0 && newJ < n 
+                               && !visited[newI][newJ] && grid[newI][newJ] > 0) {
+                                queue.offer(new int[]{newI, newJ});
+                                visited[newI][newJ] = true;
+                            }
+                        }
+                    }
+                    maxFish = Math.max(maxFish, currentFish);
                 }
             }
         }
         return maxFish;
-    }
-    
-    private int dfs(int[][] grid, int i, int j, int m, int n, boolean[][] visited) {
-        // Check bounds or if cell is already visited or is land
-        if(i < 0 || i >= m || j < 0 || j >= n || visited[i][j] || grid[i][j] == 0) {
-            return 0;
-        }
-        
-        visited[i][j] = true;
-        int currentFish = grid[i][j];
-        
-        // Visit all adjacent cells
-        currentFish += dfs(grid, i+1, j, m, n, visited); // down
-        currentFish += dfs(grid, i-1, j, m, n, visited); // up
-        currentFish += dfs(grid, i, j+1, m, n, visited); // right
-        currentFish += dfs(grid, i, j-1, m, n, visited); // left
-        
-        return currentFish;
     }
 }
